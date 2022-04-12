5d43981e75e28a14c66523ccf7aa62feebd00da0
Alone <84889578+PRONOI@users.noreply.github.com>
2022-04-12 15:50:11 +0530

    Add files via upload

---
+++ driver/core.py
@@ -0,0 +1,31 @@
+from pyrogram import Client
+from pytgcalls import PyTgCalls
+from config import API_HASH, API_ID, BOT_TOKEN, SESSION_NAME
+
+
+bot = Client(
+    ":RYTHMX:",
+    API_ID,
+    API_HASH,
+    bot_token=BOT_TOKEN,
+    plugins={"root": "program"},
+)
+
+user = Client(
+    SESSION_NAME,
+    api_id=API_ID,
+    api_hash=API_HASH,
+)
+
+calls = PyTgCalls(
+    user,
+    cache_duration=100,
+    overload_quiet_mode=True,
+)
+
+call_py = PyTgCalls(user, overload_quiet_mode=True)
+with Client(":RYTHMX:", API_ID, API_HASH, bot_token=BOT_TOKEN) as app:
+    me_bot = app.get_me()
+with user as app:
+    me_user = app.get_me()
+call_py = PyTgCalls(user, overload_quiet_mode=True)
